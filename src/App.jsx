54c37c48d3a2e1dc33305030b38d5c36fac1e8a8
ritesh-947 <Ritesh.the.rk.star.123@gmail.com>
2026-02-14 11:11:34 +0530

    blank page issue solving

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import MoneyTalk from "./Money_talk/money_talk";
 import Login from "./Login_page/Login";
-import Header from "./Header/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="app-container">
       <Routes>
-      <Route
-          path="/"
-          
-          element={<MoneyTalk />}
-        />
+        <Route path="/" element={<MoneyTalk />} />
         <Route
           path="/how-to-be-rich-effective-practical-guides"
-          
+          element={<MoneyTalk />}
         />
-
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
